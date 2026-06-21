3d6937078c4f4974b3523722adfd8775aeaffdb3
Aishwarya Shrivastava <shrivastavaishwarya34@gmail.com>
2026-06-22 02:06:52 +0530

    Create dsa-prac.js

---
+++ dsa-prac.js
@@ -0,0 +1,55 @@
+
+// function fibonacci(){
+//     let arr = [0,1]
+
+//     for (let i = 2; i < 19 ; i++){
+//         arr.push(arr[i - 2] + arr[i - 1])           // fibonacci nos
+//     }
+//     console.log(arr)
+// }
+
+// fibonacci()
+
+
+
+
+
+
+
+
+
+// function minValue(){
+//     let arr = [11,5,9,7,3,14]
+//     let minVal = arr[0]
+//     for( let i = 0 ; i < arr.length ; i++){         // minimum of the array  O(n)
+//         if(arr[i] < minVal){
+//             minVal = arr[i]
+//         }
+//     }
+//     console.log(minVal)
+// }
+
+// minValue()
+
+
+
+
+
+
+
+
+// function bubbleSort() {
+//     let arr = [7,12,9,11,3]
+//     for (let i=0 ; i<arr.length - 1 ; i++){
+//         for (let i=0 ; i<arr.length - 1 ; i++){         // bubbleSort O(n-n)
+//             if(arr[i] > arr[i + 1]){
+//                 let tempVal = arr[i]
+//                 arr[i] = arr[i + 1]
+//                 arr[i + 1] = tempVal
+//             }
+//         }
+//     }
+//     console.log(arr)
+// }
+
+// bubbleSort()
